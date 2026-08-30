021fa697d52ba022dddf0a1877ca7e97f19f6203
elprofenica505-ai <elprofenica505@gmail.com>
2026-08-30 14:16:56 -0600

    Create ComprasAdmin.tsx

---
+++ app/components/ComprasAdmin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ComprasAdmin() {
+  return (
+    <div style={{ padding: 20, color: '#fff' }}>
+      <h2>Módulo de Compras</h2>
+      {/* Aquí va toda la lógica, tablas y formularios de compras */}
+    </div>
+  );
+}
